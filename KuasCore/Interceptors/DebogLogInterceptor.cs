e67f4ed03a2f6a03d1151c374c8d35d31ea072e8
1101137142 <1101137142@gm.kuas.edu.tw>
2015-04-17 16:17:40 +0800

    HOMEWORK-Sprint.NET-AOP-1.0

---
+++ KuasCore/Interceptors/DebogLogInterceptor.cs
@@ -0,0 +1,25 @@
+﻿using AopAlliance.Intercept;
+using System;
+using System.Diagnostics;
+using KuasCore.Models;
+
+namespace KuasCore.Interceptors 
+{
+    class DebogLogInterceptor : IMethodInterceptor
+    {
+
+        public object Invoke(IMethodInvocation invocation)
+        {
+            Console.WriteLine("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+            Debug.Print("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+
+            Employee result = (Employee)invocation.Proceed();
+            result.Name += " 被...";
+
+            Console.WriteLine("回傳的資料已取得 [{0}]", result);
+            Debug.Print("回傳的資料已取得 [{0}]", result);
+
+            return result;
+        }
+    }
+}
